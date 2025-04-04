a3eda7534446b5467d2ced298b7ce13d280e1890
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-05 03:03:26 +0700

    mode_dev-prod

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -4,6 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { StatusCodes } from "http-status-codes"
+import { env } from "~/config/environment"
 // Middleware xử lý lỗi tập trung trong ứng dụng BE Nodejs(ExpressJs)
 export const errorHandlingMiddleware = (err, req, res, next) => {
   // Nếu dev ko cẩn thận thiếu status code thì mặc định để là 500
@@ -16,6 +17,9 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     message: err.message || StatusCodes[err.statusCode],
     stack: err.stack,
   }
+  // console.log("BUILD_MODE: ", env.BUILD_MODE)
+  // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn ko thì xóa đi
+  if (env.BUILD_MODE !== "dev") delete responseError.stack
   //   trả responseError cho FE
   res.status(responseError.statusCode).json(responseError)
 }
