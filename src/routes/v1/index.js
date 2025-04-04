cb69779a2e69152f223d903f0936a541a1787ba4
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-04 18:18:13 +0700

    Setting_Router_Express

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,17 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from "express"
+import { StatusCodes } from "http-status-codes"
+import { boardRoutes } from "./boardRoutes"
+
+const Router = express.Router()
+// Check APIs v1/status
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "APIs V1 are ready" })
+})
+
+Router.use("/boards", boardRoutes)
+
+export const APIs_V1 = Router
