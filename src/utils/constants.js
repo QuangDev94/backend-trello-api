d2e6d4dc47fbc725b82f66fc9539f8a324007cf8
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-16 08:09:56 +0700

    Update whiteList domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
   // "http://localhost:5173",
-  "https://backend-trello-api.onrender.com",
+  "https://trello-web-nine-chi.vercel.app/",
 ]
 
 export const BOARD_TYPES = {
