b4f9d0e3253d2541cbff2a6fe64496a11af5eca8
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-25 10:38:13 +0700

    HttpOnly-cookie_AuthMiddleware_Redux-persist_Protected-routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from "http-status-codes"
+import ms from "ms"
 import { userService } from "~/services/userService"
 
 const createNew = async (req, res, next) => {
@@ -22,6 +23,20 @@ const verify = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const loginUser = await userService.login(req.body)
+    // Xử lý trả về http only cookie cho phía trình duyệt
+    // thời gian sống maxAge của cookie khác với thời gian sống của token
+    res.cookie("accessToken", loginUser.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: ms("14 days"),
+    })
+    res.cookie("refreshToken", loginUser.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: ms("14 days"),
+    })
     res.status(StatusCodes.CREATED).json(loginUser)
   } catch (error) {
     next(error)
--- src/routes/v1/cardRoutes.js
+++ src/routes/v1/cardRoutes.js
@@ -1,8 +1,11 @@
 import express from "express"
 import { cardController } from "~/controllers/cardController"
+import { authMiddleware } from "~/middlewares/authMiddleware"
 import { cardValidation } from "~/validations/cardValidation"
 
 const Router = express.Router()
+
+Router.use(authMiddleware.isAuthorized)
 
 Router.route("/").post(cardValidation.createNew, cardController.createNew)
 
